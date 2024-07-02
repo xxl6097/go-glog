199b38d8eb554092e44faf20cdda70edc3d8f497
MacBookAirM2 <uuxia@macbookairm2.com>
2024-07-02 17:47:34 +0800

    2024-07-02 17:47:34 by uuxia

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-appversion=0.0.0
-
 function getversion() {
   appversion=$(cat version.txt)
   if [ "$appversion" = "" ]; then
@@ -10,9 +8,9 @@ function getversion() {
     v3=$(echo $appversion | awk -F'.' '{print($3);}')
     v2=$(echo $appversion | awk -F'.' '{print($2);}')
     v1=$(echo $appversion | awk -F'.' '{print($1);}')
-    if [[ $(expr $v3 \>= 9) == 1 ]]; then
+    if [[ $(expr $v3 \>= 99) == 1 ]]; then
       v3=0
-      if [[ $(expr $v2 \>= 9) == 1 ]]; then
+      if [[ $(expr $v2 \>= 99) == 1 ]]; then
         v2=0
         v1=$(expr $v1 + 1)
       else
@@ -41,6 +39,16 @@ function forcepull() {
   echo "git fetch --all && git reset --hard origin/master && git pull"
   git fetch --all && git reset --hard origin/master && git pull
 }
+
+function tag() {
+    version=$(getversion)
+    echo "current version:${version}"
+    git add .
+    git commit -m "release v${version}"
+    git tag -a v$version -m "release v${version}"
+    git push origin v$version
+    echo $version >version.txt
+}
 #  shellcheck disable=SC2120
 function gitpush() {
   commit=""
@@ -55,22 +63,20 @@ function gitpush() {
   git commit -m "$commit"
   #  git push -u origin main
   git push
+  tag
 }
 
-function gittag() {
-  appversion=$(getversion)
-  echo "当前版本：$appversion"
-  git add .
-  git commit -m "release v$appversion"
-  git tag -a v${appversion} -m "release v$appversion"
-  git push origin v${appversion}
-  echo $appversion >version.txt
+function test() {
+    version=$(getversion)
+    echo "current version:${version}"
+    echo $version >version.txt
 }
+
 function m() {
     echo "1. 强制更新"
     echo "2. 普通更新"
     echo "3. 提交项目"
-    echo "4. 提交tag"
+    echo "4. 测试"
     echo "请输入编号:"
     read index
 
@@ -78,7 +84,7 @@ function m() {
     [1]) (forcepull);;
     [2]) (pull);;
     [3]) (gitpush);;
-    [4]) (gittag);;
+    [4]) (test);;
     *) echo "exit" ;;
   esac
 }
@@ -91,6 +97,5 @@ function bootstrap() {
        *) ( gitpush $1)  ;;
     esac
 }
-
 
 bootstrap m
